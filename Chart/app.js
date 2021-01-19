5db25f189a0339c2f7a0a1140bc87262fe5f49fa
Ruoqi Huang <huangruoqi374@gmail.com>
2021-01-19 13:55:20 -0800

    done with chart

--- Chart/app.js
+++ Chart/app.js
@@ -1,13 +1,54 @@
+
+
+const drawChart = async () => {
+    const data = await getData();
+    const ctx = document.getElementById('chart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: { 
+            labels: data.years,
+            datasets: [{
+                label: 'Global Average Temperature',
+                data: data.temp,
+                fill: false,
+                backgroundColor: 
+                    'rgba(255, 99, 132, 0.2)',  
+                borderColor: 
+                    'rgba(255, 99, 132, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        callback: (value,index,values) => {
+                            return value + 'º';
+                        }
+                    }
+                }]
+            }
+        }
+    });
+}
+
+
+
+
 const getData = async () => {
     const years = [];
     const temp = [];
     const response = await fetch('ZonAnn.Ts+dSST.csv');
     const data = await response.text();
-    table = data.split('\n').slice(0);
+    table = data.split('\n').slice(1);
     table.forEach(row => {
         const columns = row.split(',');
-        years.push(conlums[-1]);
-        temp.push(parseFloat(columns[0]) + 14);
+        years.push(columns[0]);
+        temp.push(parseFloat(columns[1]) + 14);
     })
-
+    return { years, temp}
 }
+
+
+
+drawChart();
